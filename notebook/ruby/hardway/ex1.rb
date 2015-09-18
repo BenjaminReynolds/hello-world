d1c2ebf6f2a1c9cf1e6ba7375e6aaff70d33eb18
Benjamin Reynolds <breyno127@gmail.com>
2015-09-18 13:30:45 +0000

    Update file tree and remove .DS_Store files

---
+++ notebook/ruby/hardway/ex1.rb
@@ -0,0 +1,8 @@
+puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+#puts "This is fun"
+#puts "Yay! Printing."
+#puts "I'd much rather you 'not'."
+#puts 'I "said" do not touch this.'
+#puts "Here I am printing another line. I really know how to do this already, but this is what the exercise tells me to do."
