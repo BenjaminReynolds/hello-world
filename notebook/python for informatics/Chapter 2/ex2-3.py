3a3f30a90f47194dd2cf1e16875b3496feb79d62
Benjamin Reynolds <breyno127@gmail.com>
2015-10-13 15:58:37 +0000

    Work on python for informatics

---
+++ notebook/python for informatics/Chapter 2/ex2-3.py
@@ -0,0 +1,3 @@
+hours = raw_input("Enter Hours: ")
+rate = raw_input("Enter Rate: ")
+print "Pay:", round((float(hours) * float(rate)), 2)
---
+++ notebook/python for informatics/Chapter 2/ex2-5.py
@@ -0,0 +1,2 @@
+celsius = raw_input("Enter temperature in Celsius: ")
+print "Temperature in Farenheight:",((9.0 / 5.0) * float(celsius) + 32)
