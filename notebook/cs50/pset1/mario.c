c6f67c1e6a43c88553d40d77fcf705a6d4a40f05
Benjamin Reynolds <breyno127@gmail.com>
2015-11-14 16:39:12 -0500

    add cs50 files

---
+++ notebook/cs50/pset1/mario.c
@@ -0,0 +1,41 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    
+    // gets and validates users input
+    
+    do
+    {
+        printf("Height: ");
+        height = GetInt();
+    }
+    while (height < 0 || height > 23);
+    
+    // loop through every row of the pyramid, starting at the top
+    
+    for (int i = 0; i < height; i++)
+    {
+        
+        // loop  through every cell on the given row
+        
+        for (int j = 0; j <= height; j++)
+        {
+            /* decides whether to print a space or a hash 
+            depending on the current row and cell */
+            
+            if (j < height - (i + 1))
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("#");
+            }
+        }
+        
+        printf("\n");
+    }
+}
