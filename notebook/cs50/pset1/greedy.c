ebe02d74c68005ae0cf3be7647a2f676fd2841b2
Benjamin Reynolds <breyno127@gmail.com>
2015-11-16 16:24:59 +0000

    Create greedy.c and write out skeleton for solution

--- notebook/cs50/pset1/greedy.c
+++ notebook/cs50/pset1/greedy.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 int main(void)
 {
-    
+	int input;
+		
+	// gets a validates users input
+		
+	do
+	{
+		printf("Hello there user! How much change is owed");
+		input = GetFloat();
+	}
+	while (input <= 0);
+	
+	// converts input to a number of cents
+	
+	// initialize the return value
+	
+	int total = 0;
+	
+	// loop over the converted input, decreasing the value and increasing the
+	// return value with each iteration
+	
+	while (input > 0)
+	{
+		
+	}
+	
+	printf("%d\n", total);
 }
