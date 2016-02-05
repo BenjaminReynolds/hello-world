45c8e1acce502ecf7fe95f5024f345400f96cffa
Benjamin Reynolds <breyno127@gmail.com>
2016-02-05 20:53:14 +0000

    start hacker version of pset2

---
+++ notebook/cs50/hacker2/initials.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    
+    // Get the users input and store it in a variable
+    
+    string name = GetString();
+    
+    /**
+    *   Iterate over the string the user gave us one character at a time.
+    *   If the character at the current index is the first character of the
+    *   string, or the previous character is a space, print the character.
+    **/
+    for (int i = 0, n = strlen(name) ;i < n; i++)
+    {
+        // ensure that the chracter is a letter before printing it for hacker version
+        
+        if ((name[i - 1] == ' ' || i == 0) && isalpha(name[i]))
+        {
+            // If the character is already uppercased, print it as is.
+            if (isupper(name[i]))
+            {
+                printf("%c", name[i]);    
+            }
+            // otherwise, convert the character to an uppercase letter
+            else
+            {
+                printf("%c", toupper(name[i]));
+            }
+        }
+    }
+    printf("\n");
+}
