3d45e6e58d205b38aae1d054ed159c2f81f5d955
Benjamin Reynolds <breyno127@gmail.com>
2016-02-16 20:57:35 +0000

    commented generate.c in cs50 pset3

--- notebook/cs50/pset3/find/generate.c
+++ notebook/cs50/pset3/find/generate.c
@@ -24,17 +24,29 @@
 
 int main(int argc, string argv[])
 {
-    // TODO: comment me
+    /**
+     * check the number of command line arguments to ensure that the program was
+     * run correctly. If the number of command line arguments is incorrect,
+     * display a message to the user showing the correct usage of the program
+     */
     if (argc != 2 && argc != 3)
     {
         printf("Usage: generate n [s]\n");
         return 1;
     }
 
-    // TODO: comment me
+    /**
+     * convert the first command line argument (other than the name of the 
+     * program itself) from a string to an int
+     */
     int n = atoi(argv[1]);
 
-    // TODO: comment me
+    /**
+     * If a second command line argument is present (other than the name of the
+     * program itself) seed future calls of drand with that comman line argument
+     * (converted to an integer), if no seed value is given seed with the number
+     * of seconds since epoch (current time)
+     */
     if (argc == 3)
     {
         srand48((long int) atoi(argv[2]));
@@ -44,7 +56,12 @@ int main(int argc, string argv[])
         srand48((long int) time(NULL));
     }
 
-    // TODO: comment me
+    /**
+     * generate n pseodorandom numbers using a seed value of x, where n is the 
+     * first command line argument given to the program, and x is either the 
+     * second command line argument given, or the current time in seconds from 
+     * epoch (see above), print these numbers to the screen
+     */
     for (int i = 0; i < n; i++)
     {
         printf("%i\n", (int) (drand48() * LIMIT));
