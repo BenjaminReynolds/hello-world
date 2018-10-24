283defa0322bb78b050d80b4bfed58fed21bf09f
Benjamin Reynolds <breyno127@gmail.com>
2018-10-23 21:40:19 -0500

    [the-rust-book] Int list exercises from the end of chapter 8

---
+++ lang/rust/rust-book/int_lists/src/main.rs
@@ -0,0 +1,40 @@
+use std::collections::HashMap;
+
+fn main() {
+    let list_of_integers = vec![1, 5, 5, 12, 10, 7, 3];
+    mean_of_list(&list_of_integers);
+    median_of_list(&list_of_integers);
+    mode_of_list(&list_of_integers);
+}
+
+fn mean_of_list(list: &Vec<usize>) {
+    let mut total = 0;
+    for i in list.iter() {
+        total += i;
+    }
+    let mean = total / list.len();
+    println!("{}", mean);
+}
+
+fn median_of_list(list: &Vec<usize>) {
+    let mut sorted_list = list.clone();
+    sorted_list.sort();
+    let mid_point = list.len() / 2;
+    let median = sorted_list[mid_point];
+    println!("{}", median);
+}
+
+fn mode_of_list(list: &Vec<usize>) {
+    let mut counts = HashMap::new();
+    let mut most = 1;
+    let mut mode = 0;
+    for i in list.iter() {
+        let count = counts.entry(i).or_insert(1);
+        *count += 1;
+        if *count > most {
+            most = *count;
+            mode = *i;
+        }
+    }
+    println!("{}", mode);
+}
